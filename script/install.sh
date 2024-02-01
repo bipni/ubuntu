b5af3dbcedf4f49e1c43160300a32a5fb833e7fd
bipni <bipumirja@gmail.com>
2024-02-02 01:30:42 +0600

    uninstall games added

--- script/install.sh
+++ script/install.sh
@@ -2,8 +2,14 @@
 
 START_TIME=$(date +"%Y-%m-%d %I:%M:%S %p")
 
-echo "Just sit back and watch the installation process"$'\n'
+echo "Just sit back and wait until all the processes is finished"$'\n'
+sleep 3
+
 echo "Started at $START_TIME"$'\n'
+
+# Pre Installation Task
+echo "Starting Pre-Installation Tasks"$'\n'
+sleep 3
 
 sudo apt update && sudo apt upgrade -y
 
@@ -27,8 +33,15 @@ source ./programs/vlc.sh
 # Special
 # source ./programs/nvidia.sh
 
+# Post Installation Task
+echo "Starting Post-Installation Tasks"$'\n'
+sleep 3
+
+source ./post-script/games.sh
+sudo apt autoremove
+
 END_TIME=$(date +"%Y-%m-%d %I:%M:%S %p")
-echo "Finished at $END_TIME"$'\n'
+echo $'\n'"Finished at $END_TIME"$'\n'
 
 START_TIMESTAMP=$(date -d "$START_TIME" +%s)
 END_TIMESTAMP=$(date -d "$END_TIME" +%s)
@@ -37,4 +50,4 @@ TIMESTAMP_DIFFERENCE=$((END_TIMESTAMP - START_TIMESTAMP))
 
 DIFFERENCE=$(date -u -d @${TIMESTAMP_DIFFERENCE} +"%H:%M:%S")
 
-echo "Total time taken to install all programs: $DIFFERENCE"
+echo "Total time taken to complete the job: $DIFFERENCE"
